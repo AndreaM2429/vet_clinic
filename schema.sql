6adb8e6437a7d94baba3f8d025a42cf7777c9375
Andrea Manuel <andreamanuel2207@gmail.com>
2023-08-14 15:00:33 -0600

    Add the query to create the animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id int GENERATED ALWAYS AS IDENTITY,
+    name varchar(200),
+    date_of_birth date,
+    escape_attempts int,
+    neutered boolean,
+    weight_kg float
 );
